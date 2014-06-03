451d7787b9d8272010bf302792d04e3a111aaf91
Stuart Buckley <stuart.buckley@echohealthsolutions.com>
2014-06-03 11:35:00 -0500

    Changes made by Stuart Buckley (stuart.buckley@echohealthsolutions.com).

---
+++ The Green Book/scripts/Infobox.js
@@ -0,0 +1,33 @@
+﻿
+(function ($) {
+    $(document).ready(function () {
+
+        $('div[data-role="infobox-header"]').click(function () {
+
+            var tag = $(this).attr("data-tag");
+            
+            $("div[data-role='infobox-flyout']:not(div[data-tag='" + tag + "'])").slideUp('fast');
+
+            $("div[data-tag='" + tag + "'][data-role='infobox-flyout']").slideToggle('fast');
+
+            $(this).children("div.icon").children("span").toggleClass("icon-img-down");
+          
+        });
+    });
+
+})(jQuery);
+
+function myFunction()
+{
+var x;
+var r=confirm("Here is is a popup");
+if (r==true)
+  {
+  x="You pressed OK!";
+  }
+else
+  {
+  x="You pressed Cancel!";
+  }
+document.getElementById("demo").innerHTML=x;
+}
---
+++ The Green Book/scripts/pop-up.js
@@ -0,0 +1,14 @@
+
+<!-- Begin POP-UP SIZES AND OPTIONS CODE
+
+
+function popcontact(URL) {
+var popup_width = 600
+var popup_height = 400
+day = new Date();
+id = day.getTime();
+eval("page" + id + " = window.open(URL, '" + id + "', 'toolbar=0,scrollbars=1,location=0,statusbar=0,menubar=0,resizable=1,width='+popup_width+',height='+popup_height+'');");
+}
+
+
+//  End -->
